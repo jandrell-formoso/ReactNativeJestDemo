0084d1695de0a6c8fe9b1a525788fa7741a617f0
Jandrell Ian L. Formoso <jandrell.formoso@myproperty.ph>
2018-07-27 16:58:37 +0800

    Refactor RepoList-test

--- src/reducers/repoReducer.js
+++ src/reducers/repoReducer.js
@@ -1,3 +1,17 @@
-export const repoReducer = (state = {}, actions) => {
-  return state;
+import { SEARCH_GITHUB_REPO_SUCCESS } from "../components/DataList/actions";
+
+const initialState = {
+  repoList: []
+};
+
+export const repoReducer = (state = initialState, actions) => {
+  switch(actions.type) {
+    case SEARCH_GITHUB_REPO_SUCCESS:
+      return {
+        repoCount: actions.payload.total_count,
+        repoList: actions.payload.items,
+      }
+    default:
+      return state;
+  }
 }
